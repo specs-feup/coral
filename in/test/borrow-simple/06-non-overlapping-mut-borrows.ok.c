fe6a6b816fea1ea18201bce51c90e74a38c95ce7
Pedro Gonçalo Correia <goncalerta@gmail.com>
2024-02-20 03:48:54 +0000

    Fix test 06

--- in/test/borrow-simple/06-non-overlapping-mut-borrows.ok.c
+++ in/test/borrow-simple/06-non-overlapping-mut-borrows.ok.c
@@ -1,12 +1,15 @@
 int main() {
-    const int a = 5;
-    const int *restrict ptr1, *restrict ptr2;
+    int a = 5;
+    int *restrict ptr1, *restrict ptr2;
 
     ptr1 = &a;
-    const int b = *ptr1;
+    int b = *ptr1;
+    *ptr1 = *ptr1 + 4;
 
     ptr2 = &a;
-    const int c = *ptr2;
+    *ptr2 *= 2;
+    int c = *ptr2;
+    
 
     return 0;
 }
