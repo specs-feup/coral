df43ee2834d38acc882cec78a5b32c4e8390ae56
João Bispo <jbispo@fe.up.pt>
2024-08-10 00:05:11 +0100

    Small fix related to changes in Query interface

--- src/normalize/pass/SimplifyAssignments.ts
+++ src/normalize/pass/SimplifyAssignments.ts
@@ -1,7 +1,4 @@
-import {
-    BinaryOp,
-    Joinpoint,
-} from "clava-js/api/Joinpoints.js";
+import { BinaryOp, Joinpoint } from "clava-js/api/Joinpoints.js";
 import SimplifyAssignment from "clava-js/api/clava/code/SimplifyAssignment.js";
 import CoralNormalizer from "coral/normalize/CoralNormalizer";
 import { LaraJoinPoint } from "lara-js/api/LaraJoinPoint.js";
@@ -9,7 +6,7 @@ import Query from "lara-js/api/weaver/Query.js";
 
 export default class SimplifyAssignments implements CoralNormalizer.Pass {
     apply($jp: Joinpoint) {
-        const binaryOps = Query.searchFrom($jp, "binaryOp", {
+        const binaryOps = Query.searchFrom($jp, BinaryOp, {
             self: (self: LaraJoinPoint) =>
                 (self as BinaryOp).isAssignment && (self as BinaryOp).operator !== "=",
         });
