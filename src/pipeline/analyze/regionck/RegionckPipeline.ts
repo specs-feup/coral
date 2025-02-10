4eb18590eca42c3e724e2aade1c6429baa24f694
Goncalerta <goncalerta@gmail.com>
2025-02-10 20:44:22 +0000

    Make all tests pass

--- src/pipeline/analyze/regionck/RegionckPipeline.ts
+++ src/pipeline/analyze/regionck/RegionckPipeline.ts
@@ -1,30 +1,43 @@
+import CoralFunctionNode from "@specs-feup/coral/graph/CoralFunctionNode";
 import CoralFunctionWiseTransformation, { CoralFunctionWiseTransformationApplier } from "@specs-feup/coral/graph/CoralFunctionWiseTransformation";
+import CoralTransformation, { CoralTransformationApplier } from "@specs-feup/coral/graph/CoralTransformation";
 import ConstraintGenerator from "@specs-feup/coral/pipeline/analyze/regionck/ConstraintGenerator";
 import InferRegionBounds from "@specs-feup/coral/pipeline/analyze/regionck/InferRegionBounds";
 import InScopeLoansComputation from "@specs-feup/coral/pipeline/analyze/regionck/InScopeLoansComputation";
 import RegionckErrorReporting from "@specs-feup/coral/pipeline/analyze/regionck/RegionckErrorReporting";
 import UniversalRegionsErrorReporting from "@specs-feup/coral/pipeline/analyze/regionck/UniversalRegionsErrorReporting";
+import { NodeCollection } from "@specs-feup/flow/graph/NodeCollection";
 
-export default class RegionckPipeline extends CoralFunctionWiseTransformation {
-    fnApplier = RegionckPipelineApplier;
+export default class RegionckPipeline extends CoralTransformation {
+    applier = RegionckPipelineApplier;
 }
 
-class RegionckPipelineApplier extends CoralFunctionWiseTransformationApplier {
+class RegionckPipelineApplier extends CoralTransformationApplier {
     apply(): void {
-        if (this.graph.config.inferFunctionLifetimeBounds) {
-            if (this.graph.config.inferFunctionLifetimeBoundsIterationLimit === 0) {
-                // TODO
-                throw new Error("InferFunctionLifetimeBoundsIterationLimit must be greater than 0");
-            }
-            this.graph
-                .apply(new InferRegionBounds({ iterationLimit: this.graph.config.inferFunctionLifetimeBoundsIterationLimit }));
-        } else {
-            const target = this.fn;
+        for (const target of this.#getNoInferBoundsTargets()) {
             this.graph
                 .apply(new ConstraintGenerator({ target }))
                 .apply(new InScopeLoansComputation({ target }))
                 .apply(new RegionckErrorReporting({ target }))
                 .apply(new UniversalRegionsErrorReporting({ target }));
+        }
+
+        if (this.graph.config.inferFunctionLifetimeBounds) {
+            if (this.graph.config.inferFunctionLifetimeBoundsIterationLimit === 0) {
+                throw new Error("InferFunctionLifetimeBoundsIterationLimit must be greater than 0");
+            }
+            this.graph
+                .apply(new InferRegionBounds({
+                    iterationLimit: this.graph.config.inferFunctionLifetimeBoundsIterationLimit,
+                }));
+        }
+    }
+
+    #getNoInferBoundsTargets(): NodeCollection<CoralFunctionNode.Class> {
+        if (this.graph.config.inferFunctionLifetimeBounds) {
+            return this.graph.functionsToAnalyze.filter(fn => fn.inferRegionBoundsState === InferRegionBounds.FunctionState.IGNORE);
+        } else {
+            return this.graph.functionsToAnalyze;
         }
     }
 }
