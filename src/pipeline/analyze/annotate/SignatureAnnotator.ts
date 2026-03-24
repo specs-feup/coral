1fcafcbf110ed4cdd176535055a7676106695783
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2026-03-24 17:21:27 +0000

    Initialize symbol table with nullability states from signature

--- src/pipeline/analyze/annotate/SignatureAnnotator.ts
+++ src/pipeline/analyze/annotate/SignatureAnnotator.ts
@@ -12,32 +12,6 @@ class SignatureAnnotatorApplier extends CoralFunctionWiseTransformationApplier {
     apply(): void {
         const fnSymbol = this.fn.getSymbol(this.fn.jp);
 
-        let contracts: Contract[] = []
-        const raw = this.fn.jp.getUserField("coralContracts") as unknown as string | undefined;
-
-        if (raw) {
-            contracts = JSON.parse(raw) as Contract[];
-        }
-        console.log(contracts);
-        for (const param of fnSymbol.params) {
-            const paramContracts = contracts.filter(c => c.target === param.jp.name);
-
-            for (const contract of paramContracts) {
-                if (contract.isFinal) {
-                    param.finalNullability = contract.state;
-                    console.log(`[Signature] Post-condição para ${param.jp.name}: deve sair como ${contract.state}`);
-                } else {
-                    param.initialNullability = contract.state;
-                    console.log(`[Signature] Pré-condição para ${param.jp.name}: entra como ${contract.state}`);
-                }
-            }
-        }
-        
-        const returnContract = contracts.find(c => c.target === "return");
-        if (returnContract) {
-            fnSymbol.returnNullability = returnContract.state;
-            console.log(`[Signature] Função marcada com retorno ${returnContract.state}`);
-        }
 
         const regionVars = new Map<string, Region>();
         regionVars.set("%static", this.fn.staticRegion);
@@ -48,14 +22,35 @@ class SignatureAnnotatorApplier extends CoralFunctionWiseTransformationApplier {
             }
         }
 
-        // Inference is only done if there are no explicit pragmas
+        let contracts: Contract[] = [];
+        const raw = this.fn.jp.getUserField("coralContracts") as unknown as string | undefined;
+        if (raw) {
+            contracts = JSON.parse(raw) as Contract[];
+        }
+
+        this.fn.returnTy = fnSymbol.return.toTy(regionVars);
+        const returnContract = contracts.find(c => c.target === "return");
+        if (returnContract) {
+            fnSymbol.returnNullability = returnContract.state;
+        }
+
+        for (const param of fnSymbol.params) {
+            const paramContracts = contracts.filter(c => c.target === param.jp.name);
+            for (const contract of paramContracts) {
+                if (contract.isFinal) {
+                    param.finalNullability = contract.state;
+                } else {
+                    param.initialNullability = contract.state;
+                }
+            }
+            const ty = param.ty.toTy(regionVars);
+            this.fn.registerSymbol(param.jp, ty); 
+
+        }
+
         if (!fnSymbol.hasLifetimePragmas) {
             this.fn.inferRegionBoundsState = InferRegionBounds.FunctionState.NOT_VISITED;
         }
 
-        this.fn.returnTy = fnSymbol.return.toTy(regionVars);
-        for (const param of fnSymbol.params) {
-            this.fn.registerSymbol(param.jp, param.ty.toTy(regionVars));
-        }
     }
 }
